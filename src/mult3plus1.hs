ffd7a4c01f9c0b9d71c11a1eac1a239dd39402b5
Niels G. W. Serup <ngws@metanohi.name>
2023-12-10 17:31:59 +0100

    Refactor

--- src/mult3plus1.hs
+++ src/mult3plus1.hs
@@ -1,7 +1,7 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE LambdaCase #-}
 module Main where
 
+import Data.Function ((&))
 import Data.Maybe (maybe, fromMaybe, catMaybes)
 import Data.Ratio ((%), denominator)
 import System.Environment (getArgs)
@@ -12,6 +12,9 @@ takeWhileIncl f (x : xs) | f x = x : takeWhileIncl f xs
                          | otherwise = [x]
 takeWhileIncl _ [] = []
 
+isDivisibleBy2 :: Rational -> Bool
+isDivisibleBy2 x = denominator (x / 2) == 1
+
 data Div2Result a = CanDivide a
                   | CannotDivide
                   | Uncertain
@@ -21,9 +24,6 @@ class CollatzIntegral a where
   mult :: a -> Rational -> a
   plus :: a -> Rational -> a
   ge :: a -> a -> Bool
-
-isDivisibleBy2 :: Rational -> Bool
-isDivisibleBy2 x = denominator (x / 2) == 1
 
 data IntegerMod = IntegerMod { modPart :: Rational
                              , intPart :: Rational
@@ -61,24 +61,32 @@ instance CollatzIntegral IntegerMod where
       Uncertain
     where iEven = isDivisibleBy2 i
 
-  mult (IntegerMod m i) k = IntegerMod (m * k) (i * k)
-  plus (IntegerMod m i) k = IntegerMod m (i + k)
-  ge (IntegerMod m1 i1) (IntegerMod m2 i2) = m1 > m2 || (m1 == m2 && i1 >= i2)
+  mult (IntegerMod m i) k =
+    IntegerMod (m * k) (i * k)
+
+  plus (IntegerMod m i) k =
+    IntegerMod m (i + k)
+
+  ge (IntegerMod m1 i1) (IntegerMod m2 i2) =
+    m1 > m2 || (m1 == m2 && i1 >= i2)
 
 handleDiv2Result :: CollatzIntegral i => Div2Result i -> Maybe (Maybe i)
-handleDiv2Result (CanDivide a) = Just $ Just a
-handleDiv2Result CannotDivide = Just Nothing
-handleDiv2Result Uncertain = Nothing
+handleDiv2Result = \case
+  CanDivide a -> Just $ Just a
+  CannotDivide -> Just Nothing
+  Uncertain -> Nothing
 
 step :: CollatzIntegral i => i -> Maybe i
-step n = fmap (fromMaybe ((n `mult` 3) `plus` 1)) (handleDiv2Result $ divi2 n)
+step n = fromMaybe ((n `mult` 3) `plus` 1) <$> handleDiv2Result (divi2 n)
 
 stepsUntilSmaller :: CollatzIntegral i => i -> Maybe Integer
 stepsUntilSmaller n =
-  fmap (subtract 1)
-  $ foldl1 (\m1 m2 -> m1 >>= \n1 -> m2 >>= \n2 -> return (n1 + n2))
-  $ map (fmap (const 1)) $ takeWhileIncl (maybe False (`ge` n))
-  $ iterate (>>= step) $ Just n
+  Just n
+  & iterate (>>= step)
+  & takeWhileIncl (maybe False (`ge` n))
+  & map (fmap (const 1))
+  & foldl1 (\m1 m2 -> m1 >>= \n1 -> m2 >>= \n2 -> pure (n1 + n2))
+  & fmap (subtract 1)
 
 data Cur = Cur { curMod :: Integer
                , curTests :: [(Integer, Integer)]
@@ -89,30 +97,28 @@ initCur :: Cur
 initCur = Cur 2 [(1, 2)]
 
 searchStep :: Cur -> Cur
-searchStep (Cur m ts) = Cur m' ts'
+searchStep (Cur m ts) = Cur m' (concatMap findTests ts)
   where m' = m * 2
-        ts' =
-          concatMap (\(k, kmod) ->
-                       let is = [0..m' `div` kmod - 1]
-                           is' = catMaybes
-                                 $ zipWith (\i res -> case res of
-                                               Nothing -> Just i
-                                               _ -> Nothing
-                                           ) is
-                                 $ map (stepsUntilSmaller . IntegerMod (fromIntegral m')
-                                        . fromIntegral . (+ k) . (* kmod)) is
-                       in findSmallestMods is' 0 1 k kmod
-                    ) ts
-
+        indices kmod = [0..m' `div` kmod - 1]
+        findTests (k, kmod) =
+          let is = indices kmod
+              is' = map (stepsUntilSmaller . IntegerMod (fromIntegral m')
+                          . fromIntegral . (+ k) . (* kmod)) is
+                    & zipWith (\i -> \case
+                                  Nothing -> Just i
+                                  Just _ -> Nothing
+                              ) is
+                    & catMaybes
+          in findSmallestMods is' 0 1 k kmod
         findSmallestMods is offset scale k kmod
-          | all (\j -> j `elem` is) tests = [(k, kmod)]
-          | all (\j -> j `notElem` is) tests = []
+          | all (`elem` is) tests = [(k, kmod)]
+          | all (`notElem` is) tests = []
           | otherwise =
               let kmod' = kmod * 2
                   scale' = scale * 2
-              in findSmallestMods is offset scale' k kmod' ++
-                 findSmallestMods is (offset + scale) scale' (k + kmod) kmod'
-          where tests = map (+ offset) $ map (* scale) [0..m' `div` kmod - 1]
+              in findSmallestMods is offset scale' k kmod'
+                 ++ findSmallestMods is (offset + scale) scale' (k + kmod) kmod'
+          where tests = map (+ offset) $ map (* scale) (indices kmod)
 
 search :: Integer -> Cur
 search = search' initCur
@@ -124,7 +130,6 @@ curRatio (Cur m ts) = (sum $ map (\(_, kmod) -> m `div` kmod) ts) % m
 
 curRatio' :: Cur -> Double
 curRatio' = fromRational . curRatio
-
 
 main :: IO ()
 main = do
