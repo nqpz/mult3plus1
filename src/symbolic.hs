f91329e6128f18a3fe6dc4c6823d7ee2056b243d
Niels G. W. Serup <ngws@metanohi.name>
2023-12-11 20:16:43 +0100

    Refine structure a bit more
    Still need to handle the simpler cases.

--- src/symbolic.hs
+++ src/symbolic.hs
@@ -1,26 +1,37 @@
 module Main where
 
+data Tree = Tree { treeNDividedBy :: Integer
+                 , treeExp :: Expression
+                 , treeStruct :: TreeStructure
+                 }
+
 data Expression = Expression { expNs :: Integer
-                             , expConst :: Integer
+                             , expK :: Integer
                              }
 
-data TreeStructure = If { treeNDividedBy :: Integer
-                        , treeOdd :: Tree
+data TreeStructure = If { treeOdd :: Tree
                         , treeEven :: Tree
                         }
                    -- ^ A split where both children need to be successful.
                    | Success
                    -- ^ A straightforward success.
-                   | FiniteSuccess { treeNDividedBy :: Integer
-                                   , treeConst :: Integer
+                   | FiniteSuccess { treeK :: Integer
                                    , treeNs :: Integer
                                    }
-                   -- ^ A success if treeConst < treeNs * (N / treeNDividedBy).
-                   -- We assume this is a success, since there are only a finite
+                   -- ^ A success if treeK < treeNs * (N / treeNDividedBy).  We
+                   -- assume this is a success, since there are only a finite
                    -- number of integers to check if the condition does not
                    -- hold.
 
-data Tree = Tree Expression TreeStructure
+tree :: Tree
+tree = tree' 1 0 1
+  where tree' ns k d =
+          let d' = d * 2
+          in Tree { treeExp = Expression { expNs = ns, expK = k }
+                  , treeNDividedBy = d
+                  , treeStruct = If { treeOdd = tree' (ns * 2) (k + ns) d'
+                                    , treeEven = tree' (ns * 2) k d' }
+                  }
 
 main :: IO ()
 main = return ()
