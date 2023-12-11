bd64a932933c6d0dac56027c56df0031038f979a
Niels G. W. Serup <ngws@metanohi.name>
2023-12-11 20:00:05 +0100

    Make a tree type

--- src/symbolic.hs
+++ src/symbolic.hs
@@ -1,4 +1,26 @@
 module Main where
 
+data Expression = Expression { expNs :: Integer
+                             , expConst :: Integer
+                             }
+
+data TreeStructure = If { treeNDividedBy :: Integer
+                        , treeOdd :: Tree
+                        , treeEven :: Tree
+                        }
+                   -- ^ A split where both children need to be successful.
+                   | Success
+                   -- ^ A straightforward success.
+                   | FiniteSuccess { treeNDividedBy :: Integer
+                                   , treeConst :: Integer
+                                   , treeNs :: Integer
+                                   }
+                   -- ^ A success if treeConst < treeNs * (N / treeNDividedBy).
+                   -- We assume this is a success, since there are only a finite
+                   -- number of integers to check if the condition does not
+                   -- hold.
+
+data Tree = Tree Expression TreeStructure
+
 main :: IO ()
 main = return ()
